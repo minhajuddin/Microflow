bcf6ef6e1e301d14433b8784270129d555c8c83a
microflow <contactwithagartha@gmail.com>
2016-03-21 15:12:41 +0000

    fixed by Febbry

--- lib/microflow/repo.ex
+++ lib/microflow/repo.ex
@@ -1,3 +1,11 @@
+admin#---
+# Excerpted from "Programming Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix for more book information.
+#---
 defmodule Microflow.Repo do
 
   @moduledoc """
@@ -15,7 +23,7 @@ defmodule Microflow.Repo do
   end
 
   def all(Microflow.User) do
-    [%Microflow.User{id: "1", name: "admin", username: "admin", password: "elixir"},
+    [%Microflow.User{id: "1", name: "admin", username: "admin", password: "wowamazing"},
      %Microflow.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
      %Microflow.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
   end
