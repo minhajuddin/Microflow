f50ddb727355edb8c76f101763c196e783e9a8b3
microflow <contactwithagartha@gmail.com>
2016-03-25 15:33:29 +0000

    Installed CKEditor Full Version. Will Check Fonts. To install Braintree

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -3,40 +3,8 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-
-	
 CKEDITOR.editorConfig = function( config ) {
-  config.language = 'en';
-  config.uiColor = '#AADC6E';
-
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for a single toolbar row.
-config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
-
-
-	config.removeButtons = 'Cut,Undo,Redo,Anchor,Underline,Strike,Subscript,NumberedList,Outdent,BulletedList,Indent,Unlink,About,Copy,Paste';
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
-
-	
-
-	// Dialog windows are also simplified.
-	config.removeDialogTabs = 'link:advanced';
-};
-
