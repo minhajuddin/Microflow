696796227f3c7e9a27e63fb66e2b24f96ce2946e
microflow <contactwithagartha@gmail.com>
2016-03-25 14:13:55 +0000

    Started to implement CKEditor

---
+++ ckeditor/config.js
@@ -0,0 +1,42 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+
+	
+CKEDITOR.editorConfig = function( config ) {
+  config.language = 'en';
+  config.uiColor = '#AADC6E';
+
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'undo', 'clipboard' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+
+	config.removeButtons = 'Cut,Undo,Redo,Anchor,Underline,Strike,Subscript,NumberedList,Outdent,BulletedList,Indent,Unlink,About,Copy,Paste';
+};
+
+	
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+};
+
