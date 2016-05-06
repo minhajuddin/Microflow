3778364200b2d046d1eb17991e90fcf3db2b5f23
microflow <contactwithagartha@gmail.com>
2016-05-06 04:29:03 +0000

    push prod

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :microflow, Microflow.Endpoint,
+  secret_key_base: "KxrMW3nSM4zoRKIzM5+GuB7h+v3RYctPiHgSQmHdyoj8bpbCZmig5OAG9RncxxP1"
+
+# Configure your database
+config :microflow, Microflow.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "microflow_prod",
+  pool_size: 20
